5239ffffa5de896076f177cae211902d067d7dca
chandrabhansolanki <cbss1994@gmail.com>
2023-06-19 15:14:50 +0530

    settimeout and setinterval

--- udemy js course/chapter2/Diff_Arrow_and_Regular_function.js
+++ udemy js course/chapter2/Diff_Arrow_and_Regular_function.js
@@ -5,12 +5,12 @@
 
 // point 1 example
 
-function Student(name,age){
-    this.name= name
-    this.age= age
+function Student(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-const chandra = new Student("chandra", 25)
+const chandra = new Student("chandra", 25);
 console.log(chandra);
 
 // const Car = (name, color)=> {
@@ -20,17 +20,14 @@ console.log(chandra);
 
 // const maruti = new Car("swift", "red")
 
-
-
 // point 2 example
-function add(a,b){
-    console.log(arguments);// it will show the data relatede to the arguments
-    var ans = a+b
-    return ans
+function add(a, b) {
+  console.log(arguments); // it will show the data relatede to the arguments
+  var ans = a + b;
+  return ans;
 }
-const b= add(2,3)
+const b = add(2, 3);
 console.log(b);
-
 
 // const square = (a,b) => {
 //     console.log(arguments)// it will show reference error
@@ -41,18 +38,16 @@ console.log(b);
 
 // point 3 example
 
-person() 
+person();
 
-function person (){
-    console.log("regular function");
+function person() {
+  console.log("regular function");
 }
-
 
 // console.log(person1());
 // const person1 = () => {
 //     console.log("arrow function");
 // }
-
 
 // point 4 example
 
@@ -65,31 +60,32 @@ function person (){
 
 // const data = userInfo.fullName()
 
+const userInfo = {
+  name: "chandra",
+  fullName: () => {
+    var a = () => {
+      console.log(this.name + " " + `Singh`);
+    };
+    a();
+  },
+};
 
-// const userInfo = {
-//     name : "chandra",
-//     fullName:()=>{
-//         var a = ()=> {
-//             console.log(this.name + ' '+ `Singh`);
-//         }
-//         a();
-//     }
-// }
-
-// userInfo.fullName()
-
- 
+userInfo.fullName();
 
 // this is a bug in java script function where 'this' will refer to the global object when we use 'this' inside a lexical function. In these condition we have to use arrow function inside a function so we can get the object property.
 
 // const userInfo = {
-//     name : "chandra",
-//     fullName: function(){
-//         function a () {
-//             console.log(this);
-//         }
-//         a();
+//   name: "chandra",
+//   fullName: function () {
+//     function a() {
+//       console.log(this);
 //     }
-// }
+//     a();
+//   },
+// };
 
-// userInfo.fullName()
+// userInfo.fullName();
+
+var a = 4;
+let as = 9;
+console.log(a);
---
+++ udemy js course/chapter2/SetTimeOut_and _setInterval.js
@@ -0,0 +1,2 @@
+// SetTimeout = setTimout function call a function after a certain period of Time.
+// setinterval = method is used to call a function repeatedly at a specified interval of time.
